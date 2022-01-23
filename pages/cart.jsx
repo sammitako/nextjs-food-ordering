3cf897b06eef567da073f574c234a11cb5025ff3
Hansaem Park <hansaempark@sunssc.com>
2022-01-24 08:50:55 +0900

    App Design

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -43,6 +43,35 @@ const Cart = () => {
               <span className={styles.total}>$39.80</span>
             </td>
           </tr>
+          <tr className={styles.tr}>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image
+                  src="/img/pizza.png"
+                  layout="fill"
+                  objectFit="cover"
+                  alt=""
+                />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>CORALZO</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredient, spicy sauce
+              </span>
+            </td>
+            <td>
+              <span className={styles.price}>$19.90</span>
+            </td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>$39.80</span>
+            </td>
+          </tr>
         </table>
       </div>
       <div className={styles.right}>
